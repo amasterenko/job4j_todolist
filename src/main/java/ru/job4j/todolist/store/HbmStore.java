4f38f239e28af1ebd0c5422fd2dc5a324fa94cae
Andrey Masterenko <masterenko.as@gmail.com>
2021-04-26 11:45:52 +0300

    3. Лямбды и шаблон wrapper. [#466402]

--- src/main/java/ru/job4j/todolist/store/HbmStore.java
+++ src/main/java/ru/job4j/todolist/store/HbmStore.java
@@ -2,12 +2,14 @@ package ru.job4j.todolist.store;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import ru.job4j.todolist.model.Item;
 
 import java.util.List;
+import java.util.function.Function;
 
 /**
  * The class implements Store using Hibernate framework.
@@ -17,9 +19,9 @@ import java.util.List;
  */
 
 public class HbmStore implements Store {
-    private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+    private final StandardServiceRegistry REGISTRY = new StandardServiceRegistryBuilder()
             .configure().build();
-    private final SessionFactory sf = new MetadataSources(registry)
+    private final SessionFactory SF = new MetadataSources(REGISTRY)
             .buildMetadata().buildSessionFactory();
 
     public static Store instOf() {
@@ -30,38 +32,44 @@ public class HbmStore implements Store {
         private final static Store INST = new HbmStore();
     }
 
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = SF.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
+    }
+
     @Override
     public Item add(Item item) {
-        try(Session session = sf.openSession()) {
-            session.beginTransaction();
+        return this.tx(session -> {
             session.save(item);
-            session.getTransaction().commit();
-        }
-        return item;
+            return item;
+        });
     }
 
     @Override
     public List<Item> findAll() {
-        List<Item> result = List.of();
-        try (Session session = sf.openSession()) {
-            session.beginTransaction();
-            result = session.createQuery("from ru.job4j.todolist.model.Item").list();
-            session.getTransaction().commit();
-        }
-        return result;
+        return this.tx(session -> session.createQuery("from Item").list());
     }
 
     @Override
     public void update(Item item) {
-        try(Session session = sf.openSession()) {
-            session.beginTransaction();
+        this.tx(session -> {
             session.update(item);
-            session.getTransaction().commit();
-        }
+            return item;
+        });
     }
 
     @Override
     public void close() {
-        StandardServiceRegistryBuilder.destroy(registry);
+        StandardServiceRegistryBuilder.destroy(REGISTRY);
     }
 }
